45ebdbc44ee94a20241befabb0e15595cd2e01ee
Niklhausser <jnhaussaire@gmail.com>
2023-05-25 15:20:39 +0200

    AWS port issue

--- enviroment.js
+++ enviroment.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports.enviroment = {
-    port: 3000,
+    PORT: 3000,
     pageSize: 10,
     logLevel:  'debug',
     dbConn: path.resolve(__dirname, './test.db')
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const api = require('./api.js')
-const { port } = require('../enviroment').enviroment
+const { PORT } = require('../enviroment').enviroment
 const logger = require('../utils/logger')
-api.listen(port ?? 3000, ()=> logger.info('Server listening on port '+port))
+api.listen(PORT ?? 8080, ()=> logger.info('Server listening on port '+PORT))
 module.exports = api
