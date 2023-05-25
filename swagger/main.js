e5d675ebfe3022071dc9dcff8d3f832d55a3aa2b
Niklhausser <jnhaussaire@gmail.com>
2023-05-25 15:33:07 +0200

    aws swagger

--- swagger/main.js
+++ swagger/main.js
@@ -10,7 +10,8 @@ module.exports.swaggerDocument = {
         description: pkg.description,
         termsOfService: '',
         contact: { name: 'Nicolas Haussaire', email: 'jnhaussaire@hotmail.com' }   },
-    servers: [{  url: 'http://localhost:3000', description: 'Local server' }],
+    servers: [{  url: 'http://localhost:3000', description: 'Local server' },
+                {  url: 'http://iskaypet-niko-api.eu-west-3.elasticbeanstalk.com/', description: 'ASW cloud test server' }],
     
     paths: {
         '/pets':{get: pets.getAll, post: pets.saveOrUpdate},
