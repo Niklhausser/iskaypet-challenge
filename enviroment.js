d72df7d66787556cf335aa248f0db23b51c9fee4
Niklhausser <jnhaussaire@gmail.com>
2023-05-25 14:50:49 +0200

    env4aws

--- enviroment.js
+++ enviroment.js
@@ -1,8 +1,8 @@
 const localEnv = !process.env.PORT? require('./env.local').enviroment : null
 
 module.exports.enviroment = {
-    port: process.env.PORT || localEnv.port,
-    pageSize: process.env.PAGE_SIZE || localEnv.pageSize,
-    logLevel: process.env.LOG_LEVEL || localEnv.logLevel,
-    dbConn: process.env.DBCONN || localEnv.dbConn
+    port: 3000,
+    pageSize: 10,
+    logLevel:  'debug',
+    dbConn: path.resolve(__dirname, './test.db')
 }
